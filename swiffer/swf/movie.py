f39916b8fe994bbdeda9e2c7b9cdf68e2bcf1414
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2021-04-07 05:47:02 -0700

    catch zlib.errors

--- swiffer/swf/movie.py
+++ swiffer/swf/movie.py
@@ -8,13 +8,17 @@ from .export import SVGExporter
 from six.moves import cStringIO
 from io import BytesIO
 
+
 class SWFHeaderException(Exception):
     """ Exception raised in case of an invalid SWFHeader """
+
     def __init__(self, message):
-         super(SWFHeaderException, self).__init__(message)
+        super(SWFHeaderException, self).__init__(message)
+
 
 class SWFHeader(object):
     """ SWF header """
+
     def __init__(self, stream):
         a = stream.readUI8()
         b = stream.readUI8()
@@ -46,17 +50,17 @@ class SWFHeader(object):
     def frame_count(self):
         """ Return number of frames """
         return self._frame_count
-                
+
     @property
     def file_length(self):
         """ Return uncompressed file length """
         return self._file_length
-                    
+
     @property
     def version(self):
         """ Return SWF version """
         return self._version
-                
+
     @property
     def compressed(self):
         """ Whether the SWF is compressed """
@@ -71,7 +75,7 @@ class SWFHeader(object):
     def compressed_lzma(self):
         """ Whether the SWF is compressed using LZMA """
         return self._compressed_lzma
-        
+
     def __str__(self):
         return "   [SWFHeader]\n" + \
             "       Version: %d\n" % self.version + \
@@ -80,45 +84,47 @@ class SWFHeader(object):
             "       FrameRate: %d\n" % self.frame_rate + \
             "       FrameCount: %d\n" % self.frame_count
 
+
 class SWF(SWFTimelineContainer):
     """
     SWF class
-    
-    The SWF (pronounced 'swiff') file format delivers vector graphics, text, 
+
+    The SWF (pronounced 'swiff') file format delivers vector graphics, text,
     video, and sound over the Internet and is supported by Adobe Flash
-    Player software. The SWF file format is designed to be an efficient 
-    delivery format, not a format for exchanging graphics between graphics 
+    Player software. The SWF file format is designed to be an efficient
+    delivery format, not a format for exchanging graphics between graphics
     editors.
-    
+
     @param file: a file object with read(), seek(), tell() methods.
     """
+
     def __init__(self, file=None):
         super(SWF, self).__init__()
         self._data = None if file is None else SWFStream(file)
         self._header = None
         if self._data is not None:
             self.parse(self._data)
-    
+
     @property
     def data(self):
         """
         Return the SWFStream object (READ ONLY)
         """
         return self._data
-    
+
     @property
     def header(self):
         """ Return the SWFHeader """
         return self._header
-        
+
     def export(self, exporter=None, force_stroke=False):
         """
-        Export this SWF using the specified exporter. 
-        When no exporter is passed in the default exporter used 
+        Export this SWF using the specified exporter.
+        When no exporter is passed in the default exporter used
         is swf.export.SVGExporter.
-        
+
         Exporters should extend the swf.export.BaseExporter class.
-        
+
         @param exporter : the exporter to use
         @param force_stroke : set to true to force strokes on fills,
                               useful for some edge cases.
@@ -129,15 +135,15 @@ class SWF(SWFTimelineContainer):
         if len(self.tags) == 0:
             raise Exception("This SWF doesn't contain any tags!")
         return exporter.export(self, force_stroke)
-            
+
     def parse_file(self, filename):
         """ Parses the SWF from a filename """
         self.parse(open(filename, 'rb'))
-        
+
     def parse(self, data):
-        """ 
+        """
         Parses the SWF.
-        
+
         The @data parameter can be a file object or a SWFStream
         """
         self._data = data = data if isinstance(data, SWFStream) else SWFStream(data)
@@ -148,10 +154,13 @@ class SWF(SWFTimelineContainer):
                 import zlib
                 data = data.f.read()
                 zip = zlib.decompressobj()
-                temp.write(zip.decompress(data))
+                try:
+                    temp.write(zip.decompress(data))
+                except zlib.error:
+                    temp.write(data)
             else:
                 import pylzma
-                data.readUI32() #consume compressed length
+                data.readUI32()  # consume compressed length
                 data = data.f.read()
                 temp.write(pylzma.decompress(data))
             temp.seek(0)
@@ -160,11 +169,10 @@ class SWF(SWFTimelineContainer):
             self._header._frame_rate = data.readFIXED8()
             self._header._frame_count = data.readUI16()
         self.parse_tags(data)
-        
+
     def __str__(self):
         s = "[SWF]\n"
         s += self._header.__str__()
         for tag in self.tags:
             s += tag.__str__() + "\n"
         return s
-        
